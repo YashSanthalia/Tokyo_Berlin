0dfcf2b4a1c4e5c1ddb9a5fb8bd30c1b1032b29e
Yash Santhalia <loveumommt2l@gmail.com>
2021-12-22 18:17:33 +0530

    Routes Added

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "../home/HomePage";
+import ErrorPage from "../error/ErrorPage";
+import SuperAdminPage from "../superAdmin/SuperAdminPage";
+import StudentPage from "../students/StudentPage";
+import HostelPage from "../hostels/HostelPage";
+import CanteenPage from "../canteens/CanteenPage";
+import LoginPage from "../login/LoginPage";
 
 const App = () => {
-    return <h1>Welcome!</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = { <HomePage /> } />
+        <Route path = "/login/:as" element = { < LoginPage /> } />
+        <Route path = "/superAdmin" element = { <SuperAdminPage /> } />
+        <Route path = "/hostels/:hostelName" element = { <HostelPage /> } />
+        <Route path = "/students/:regNo" element = { <StudentPage />} />
+        <Route path = "/canteens/:canteenName" element = { <CanteenPage/> } />
+        <Route path = "*" element = { <ErrorPage /> } />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/hostels/HostelPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HostelPage = () => {
+    return <h1>Hostel Page</h1>
+}
+
+export default HostelPage;
